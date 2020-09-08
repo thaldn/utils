4742816f2b496f8b9254037b7747baad73f3e5c5
Cui Jian <jian.a.cui@nokia-sbell.com>
2020-09-08 00:33:29 +0000

    add installer for ag--silver search

---
+++ ag_install.sh
@@ -0,0 +1,19 @@
+os="`python -mplatform | grep Ubuntu`"
+if [ $os ]
+then
+    sudo apt-get install automake liblzma-dev
+else
+    sudo yum install -y xz-devel zlib-devel pcre-devel automake
+fi
+
+wget https://github.com/ggreer/the_silver_searcher/archive/master.zip
+
+mv master.zip ag.zip
+
+unzip ag.zip
+
+cd the_silver_searcher-master/
+
+./build.sh
+
+sudo make install
